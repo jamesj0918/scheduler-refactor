6c328bf4eb45cdaa634ea77bb0dfe7549ff5c8d3
James <jamesj0107@naver.com>
2018-12-03 23:30:38 +0900

    Create initial router

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+//Components
+import OptionSelect from './components/OptionSelect'
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'OptionSelect',
+            component: OptionSelect
+        }
+    ]
+})
