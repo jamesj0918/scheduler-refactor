ceb5d6cee024e34224c1f84e81e908603592f7f1
James <jamesj0107@naver.com>
2018-12-05 03:14:53 +0900

    Init OptionSelectLayout.vue and add components TimeTable, OptionSelect

---
+++ src/components/OptionSelectLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="OptionSelectLayoutWrap">
+        <TimeTable></TimeTable>
+        <OptionSelect></OptionSelect>
+    </div>
+</template>
+
+<script>
+    import TimeTable from './TimeTable'
+    import OptionSelect from "./OptionSelect";
+    export default {
+        name: "OptionSelectLayout",
+        components:{
+            TimeTable,
+            OptionSelect
+        }
+    }
+</script>
+
+<style scoped>
+    #OptionSelectLayoutWrap{
+        margin: 0 auto;
+    }
+</style>
