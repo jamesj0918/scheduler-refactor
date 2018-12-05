274b261481e2de3c6d29e101f28a7824c8b31d0f
James <jamesj0107@naver.com>
2018-12-05 19:34:49 +0900

    Init SubCategory and LectureList

---
+++ src/components/SubCategory.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "SubCategory",
+        data(){
+            return{
+                category: this.$route.params.category
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
