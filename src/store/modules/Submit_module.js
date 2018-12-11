2ad6623fe926bf3ebbe50883b1b79681e248c88b
James <jamesj0107@naver.com>
2018-12-12 00:33:28 +0900

    Add submit modules on vuex for submission validation

---
+++ src/store/modules/Submit_module.js
@@ -0,0 +1,9 @@
+const state ={
+    pinned_counts: 0,
+    selected_counts: 0,
+    breaktime_counts: 0,
+};
+
+export default {
+    state
+}
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import timeTable from './modules/TimeTable_module'
+import submit from './modules/Submit_module'
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
         timeTable,
+        submit
     }
 });
 
